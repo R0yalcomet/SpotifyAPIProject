b57213d42920aa14d931590cd41c6e3e09dd0c5f
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-05-05 13:21:21 -0500

    "Construct Track component folder w/ css"

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './Track.module.css';
 
-const Track = () => {
+const Track = (props) => {
 
     return (
-        <div>
-
+        <div className={styles.Track}>
+            <div className={styles["Track-information"]}>
+                <h3>{props.name}</h3>
+                <p>{props.artist} | {props.album}</p>
+            </div>
+            <button className={styles["Track-action"]}>{props.isRemoval ? "-" : "+"}</button>
         </div>
     );
 };
