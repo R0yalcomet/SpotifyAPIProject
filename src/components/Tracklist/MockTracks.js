9360498174b741fb24b8a45a948c76f47a843017
R0yalcomet <149019857+R0yalcomet@users.noreply.github.com>
2024-05-04 20:17:48 -0500

    "Construct Tracklist component folder w/ css & mock data"

---
+++ src/components/Tracklist/MockTracks.js
@@ -0,0 +1,28 @@
+const MockTracks = [
+    {
+        album: "Vampire Disco",
+        name: "Vampire Disco",
+        artists: [{
+            name: "Friday Pilots Club"
+        }],
+        id: "abcdefg1"
+    },
+    {
+        album: "The Dead Come Talking",
+        name: "The Dead Come Talking",
+        artists: [{
+            name: "Roe Kapara"
+        }],
+        id: "abcdefg2"
+    },
+    {
+        album: "I Hope Hell Isn't Real",
+        name: "Preacher",
+        artists: [{
+            name: "Roe Kapara"
+        }],
+        id: "abcdefg3"
+    }
+];
+
+export default MockTracks;
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './Tracklist.module.css';
+import Track from '../Track/Track';
+import MockTracks from './MockTracks';
 
 const Tracklist = () => {
 
     return (
-        <div>
-
+        <div className={styles.Tracklist}>
+            {MockTracks.map(track => 
+                <Track name={track.name} artist={track.artists[0].name} album={track.album}/>
+            )}
         </div>
     );
 };
